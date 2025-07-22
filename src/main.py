fe2537845b12f56489e9fc880f860765c9dd9a2f
entfane <nik77kez@gmail.com>
2025-07-22 19:49:36 +0200

    Modified PPOTrainer

--- src/main.py
+++ src/main.py
@@ -1,23 +1,16 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+
+from reward_model import RewardModel
 
 class PPOTrainer:
 
     def __init__(self, model_name, reward_model_name):
         self.policy = AutoModelForCausalLM.from_pretrained(model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.reward_model = AutoModelForSequenceClassification.from_pretrained(reward_model_name, device_map = "auto", num_labels=1)
-        self.reward_model_tokenizer = AutoTokenizer.from_pretrained(reward_model_name)
+        self.reward_model = RewardModel(reward_model_name)
 
-    def get_reward(self, prompt, response):
-        conv = [{"role": "user", "content": prompt},
-                {"role": "assistant", "content": response}]
-        input = self.reward_model_tokenizer.apply_chat_template(conv, tokenize = False)
-        input = self.reward_model_tokenizer(input, return_tensors="pt").to(self.reward_model.device)
-        with torch.no_grad():
-            score = self.reward_model(**input)
-        return score.logits[0][0]
-    
+
 
 
 
