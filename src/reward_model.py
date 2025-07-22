1a344b39e976bd83dc53aabcd5c30542f47d2299
entfane <nik77kez@gmail.com>
2025-07-22 19:49:13 +0200

    Created Reward Model class and method to generate rewards

---
+++ src/reward_model.py
@@ -0,0 +1,22 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+from typing import List, Tuple
+
+class RewardModel:
+
+    def __init__(self, reward_model_name: str):
+        self.model = AutoModelForSequenceClassification.from_pretrained(reward_model_name, device_map = "auto", num_labels=1)
+        self.tokenizer = AutoTokenizer.from_pretrained(reward_model_name)
+
+    def get_reward(self, prompt_response_pairs: List[Tuple[str, str]]):
+        inputs = []
+        for prompt, response in prompt_response_pairs:
+            conv = [{"role": "user", "content": prompt},
+                    {"role": "assistant", "content": response}]
+            input = self.tokenizer.apply_chat_template(conv, tokenize = False)
+            inputs.append(input)
+        inputs = self.tokenizer(inputs, return_tensors="pt", padding = True).to(self.model.device)
+        with torch.no_grad():
+            score = self.model(**inputs)
+            print(score.logits)
+        return score.logits[0][0]
