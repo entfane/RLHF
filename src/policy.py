1239e0775677ecd99464a24cb82595f582c377bb
entfane <nik77kez@gmail.com>
2025-07-23 11:57:44 +0200

    Created Offline Policy with value head and frozen other layers

---
+++ src/policy.py
@@ -0,0 +1,35 @@
+from trl import AutoModelForCausalLMWithValueHead
+from transformers import AutoTokenizer
+
+VALUE_HEAD_NAME = "v_head"
+
+class OfflinePolicy:
+
+    def __init__(self, model_name):
+        self.model = AutoModelForCausalLMWithValueHead.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.__freeze_policy_without_value_head()
+
+    def __freeze_policy_without_value_head(self):
+        for name, param in self.model.named_parameters():
+            if not VALUE_HEAD_NAME in name:
+                param.requires_grad = False
+
+    def generate(self, input):
+        formatted_inputs = []
+        for prompt, completion in input:
+            chat_formatted_prompt_completion = [{'role': 'user', 'content': prompt},
+                                                {'role': 'assistant', 'content': completion}]
+            formatted_inputs.append(self.tokenizer.apply_chat_template(chat_formatted_prompt_completion), tokenize = False)
+        input = self.tokenizer(formatted_inputs, return_tensors = "pt")
+        lm_logits, _, value = self.model(**input)
+        return lm_logits, value
+
+
+
+
+# input = [{'role': 'user', 'content': 'Hey, how are you?'}, {'role': 'assistant', 'content': 'I am good'}]
+# input = tokenizer.apply_chat_template(input, tokenize = False)
+# input = tokenizer(input, return_tensors = "pt")
+# lm_logits, _, value = model(**input)
+# print(value)
