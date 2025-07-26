740aa9fe2597c3c40c1cd73863911c5b51fde092
entfane <nik77kez@gmail.com>
2025-07-26 21:48:11 +0200

    Added number of completions per prompt in trajectory generation

--- src/policy.py
+++ src/policy.py
@@ -16,14 +16,14 @@ class OfflinePolicy:
             if not VALUE_HEAD_NAME in name:
                 param.requires_grad = False
 
-    def generate(self, inputs: List[str]):
+    def generate(self, inputs: List[str], num_completions_per_prompt: int = 2):
         formatted_inputs = []
         for prompt in inputs:
             chat_formatted_prompt_completion = [{'role': 'user', 'content': prompt}]
             chat_formatted_prompt_completion = self.tokenizer.apply_chat_template(chat_formatted_prompt_completion, tokenize = False)
             formatted_inputs.append(chat_formatted_prompt_completion)
         input = self.tokenizer(formatted_inputs, return_tensors = "pt", padding = True, padding_side = 'left')
-        output = self.model.generate(**input, max_new_tokens=10, do_sample=True, top_p=0.95)
+        output = self.model.generate(**input, max_new_tokens=10, do_sample=True, top_p=0.95, num_return_sequences = num_completions_per_prompt)
         return self.tokenizer.batch_decode(output)
 
     def generate_logits_and_values(self, input: List[Tuple[str, str]]):
