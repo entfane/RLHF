11722c736e2dba991f436cbfda53d4853e867ad1
entfane <nik77kez@gmail.com>
2025-07-27 21:44:03 +0200

    Changed model to qwen3-0.6B

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from ppo_trainer import PPOTrainer
 from datasets import load_dataset
 
 if __name__ == "__main__":
-    offline_policy = OfflinePolicy("HuggingFaceTB/SmolLM2-135M-Instruct")
+    offline_policy = OfflinePolicy("Qwen/Qwen3-0.6B")
     trainer = PPOTrainer(offline_policy)
     prompts = load_dataset("data-is-better-together/10k_prompts_ranked", split = "train[:2]")
     prompts = prompts['prompt']
