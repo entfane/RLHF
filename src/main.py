c7766041a84582d85a878e2dd35e27cb12bc4454
entfane <nik77kez@gmail.com>
2025-11-01 17:55:53 +0100

    Finalized extractions of completions only

--- src/main.py
+++ src/main.py
@@ -4,9 +4,15 @@ from datasets import load_dataset
 from reward_model import RewardModel
 import torch
 
-# TODO
-def separate_prompt_and_completions(input: str, assistant_token: str):
-    pass
+def get_completions_only(trajectories, prompts, tokenizer):
+    completions = []
+    for (prompt, trajectory) in zip(prompts, trajectories):
+        prompt = tokenizer.apply_chat_template([{'role': 'user', 'content': prompt}],
+                                                                                    tokenize = False, add_generation_prompt = True)
+        completion = trajectory.replace(prompt, "")
+        completions.append(completion)
+    return completions
+    
 
 if __name__ == "__main__":
     offline_policy = Policy("HuggingFaceTB/SmolLM2-135M-Instruct")
@@ -17,13 +23,9 @@ if __name__ == "__main__":
     prompts = load_dataset("data-is-better-together/10k_prompts_ranked", split = "train[:2]")
     prompts = prompts['prompt']
     trajectories = trainer.generate_trajectories(prompts)
-    # offline_logits, _ = offline_policy.generate_logits_and_values(trajectories)
-    # online_logits, values = online_policy.generate_logits_and_values(trajectories)
-    prefix = "<|im_start|>assistant"
-    chat_formatted_prompt_completion = [{'role': 'user', 'content': prompts[0]}]
-    chat_formatted_prompt_completion = offline_policy.tokenizer.apply_chat_template(chat_formatted_prompt_completion,
-                                                                                    tokenize = True,
-                                                                                    add_generation_prompt = True)
-    print(torch.tensor(offline_policy.tokenizer(trajectories[0])['input_ids']))
-    print("########")
-    print(torch.tensor(offline_policy.tokenizer(trajectories[0])['input_ids'])[torch.tensor(chat_formatted_prompt_completion).size(0):])
+    offline_logits, _ = offline_policy.generate_logits_and_values(trajectories)
+    online_logits, values = online_policy.generate_logits_and_values(trajectories)
+    completions_only = get_completions_only(trajectories, prompts, offline_policy.tokenizer)
+    tokenized_completions = offline_policy.tokenizer(completions_only, return_tensors = "pt", padding = True)
+    print(tokenized_completions)
+
