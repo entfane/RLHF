992afb1ff4f71c7e5a2f4da608aa799b2f221420
entfane <nik77kez@gmail.com>
2025-12-12 21:49:14 +0100

    Rewrote main

--- src/main.py
+++ src/main.py
@@ -3,9 +3,14 @@ from datasets import load_dataset
 from reward_model import RewardModel
 import torch
 from trl import AutoModelForCausalLMWithValueHead
+from transformers import AutoTokenizer
     
 
 if __name__ == "__main__":
     policy = AutoModelForCausalLMWithValueHead.from_pretrained("HuggingFaceTB/SmolLM2-135M-Instruct")
+    tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-135M-Instruct")
     reward_model = RewardModel("Skywork/Skywork-Reward-V2-Qwen3-0.6B")
-    print(reward_model.get_reward([("Hey, how are you?", "Hey, i am good"), ("What's up?", "All good, dawg!")]))
+    trainer = PPOTrainer(policy, tokenizer)
+    inputs = ["how are you?", "How old are you? How is your life going?"]
+    batches = trainer.create_chat_batch_from_prompts(inputs)
+    outputs = trainer.rollout(batches)
