5c9376a9d48b93e39fc49998245b801a430f5893
entfane <nik77kez@gmail.com>
2025-12-12 21:49:05 +0100

    Modified PPO trainer

--- src/ppo_trainer.py
+++ src/ppo_trainer.py
@@ -1,13 +1,33 @@
 from typing import List
-
-from policy import Policy
+import torch
 
 class PPOTrainer:
 
-    def __init__(self, offline_policy: Policy):
-        self.offline_policy = offline_policy
+    def __init__(self, policy, tokenizer):
+        self.policy = policy
+        self.tokenizer = tokenizer
 
+    def create_chat_batch_from_prompts(self, prompts: List[str]):
+        """
+        Generates chat formated prompts from simple prompts
+        :param prompts: List of prompts
+        :type prompts: List[str]
+        """
+        chat_formatted_prompts = []
+        for prompt in prompts:
+            messages = [{"role": "user", "content": prompt}]
+            chat_formatted_prompt = self.tokenizer.apply_chat_template(messages, 
+                                                                       add_generation_prompt = True,
+                                                                       tokenize = False)
+            chat_formatted_prompts.append(chat_formatted_prompt)
+        return chat_formatted_prompts
 
-    def generate_trajectories(self, prompts: List[str]):
-        output = self.offline_policy.generate(prompts)
-        return output
+    def rollout(self, inputs, max_new_tokens = 10):
+        """
+        Performs rollout on input batch
+        
+        :param inputs: List of chat formatted inputs
+        """
+        tokenized_inputs = self.tokenizer(inputs, padding = 'longest', padding_side = "left", return_tensors = "pt")
+        outputs = self.policy.generate(**tokenized_inputs, max_new_tokens = max_new_tokens)
+        return outputs
