5f8d915fff562a9f5b2aac81988355e71e24fdfa
entfane <nik77kez@gmail.com>
2025-12-16 15:10:23 +0100

    edited test main file

--- src/main.py
+++ src/main.py
@@ -10,8 +10,14 @@ if __name__ == "__main__":
     policy = AutoModelForCausalLMWithValueHead.from_pretrained("HuggingFaceTB/SmolLM2-135M-Instruct")
     tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-135M-Instruct")
     reward_model = RewardModel("Skywork/Skywork-Reward-V2-Qwen3-0.6B")
-    trainer = PPOTrainer(policy, tokenizer)
+    trainer = PPOTrainer(policy, tokenizer, reward_model)
     inputs = ["how are you?", "How old are you? How is your life going?"]
     batches = trainer.create_chat_batch_from_prompts(inputs)
+    tokenized_inputs = tokenizer(batches, padding = 'longest', padding_side = "left", return_tensors = "pt")["input_ids"]
+    _, T = tokenized_inputs.shape
     outputs = trainer.rollout(batches, max_new_tokens=128)
+    completions_only = tokenizer.batch_decode(outputs[:, T:], skip_special_tokens = True)
+    # print(completions_only)
+    rewards = trainer.reward_model.get_reward(zip(inputs, completions_only))
+    print(trainer.get_completion_only_rewards(batches, outputs, rewards))
     print(trainer.get_completion_only_values(batches, outputs))
