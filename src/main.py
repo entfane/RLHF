1caab57e3f2d8d0e064386c4262d8fa13a46ad65
entfane <nik77kez@gmail.com>
2026-01-15 13:25:29 +0100

    Removed test break from rollouts loop in train method

--- src/main.py
+++ src/main.py
@@ -20,9 +20,10 @@ class RLHFArguments:
     max_new_tokens: int = field(default = 128, metadata={"help": "Maximum new tokens to be generated in rollouts"})
 
 if __name__ == "__main__":
-    policy = AutoModelForCausalLMWithValueHead.from_pretrained("HuggingFaceTB/SmolLM2-135M-Instruct")
+    policy = AutoModelForCausalLMWithValueHead.from_pretrained("HuggingFaceTB/SmolLM2-135M-Instruct", device_map="cuda")
     tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-135M-Instruct")
     reward_model = RewardModel("Skywork/Skywork-Reward-V2-Qwen3-0.6B")
     dataset = load_dataset("TuringEnterprises/Turing-Open-Reasoning", split = "train")
     trainer = PPOTrainer(policy, tokenizer, reward_model)
-    trainer.train(iterations=1, dataset = dataset, batch_sampling_percentage=0.1, mini_batch_size=4, epochs = 1, max_new_tokens = 16, prompt_col_name="question")
+    trainer.train(iterations=1, dataset = dataset, batch_sampling_percentage=0.1, mini_batch_size=4,
+                  epochs = 1, max_new_tokens = 16, prompt_col_name="question", gamma = 0.1, lmbda = 0.1, epsilon=0.1)
